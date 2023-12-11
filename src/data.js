11462c01a56b70b5d95ff15341d0430c15c5f67f
Gabriel <122808379+ajayigabriel@users.noreply.github.com>
2023-12-11 13:10:45 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+ const data = { 
+    products: [
+        {
+            id: '1',
+            name: 'Event',
+            price: 15000,
+            image:'https://res.cloudinary.com/dndqd8roa/image/upload/v1702050999/YBK%20COLLECTIONS/kwvufarmvb0xpw9atpal.jpg'
+        },
+        
+            {
+                id: '2',
+                name: 'Wrist',
+                price:10000,
+                image: 'https://res.cloudinary.com/dndqd8roa/image/upload/v1702050969/YBK%20COLLECTIONS/z2dctpxh5hgze1wldwav.jpg'
+            },
+        
+            {
+                id: '3',
+                name: 'Bridal',
+                price: 25000,
+                image: 'https://res.cloudinary.com/dndqd8roa/image/upload/v1702050873/YBK%20COLLECTIONS/nron2epcy2ixm8tnb2cq.jpg',
+            },
+    ],
+    
+ };
+  export default data;
